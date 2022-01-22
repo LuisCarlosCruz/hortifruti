2c4c6bfdd42fca82b6715d446199a4a13a70d5d2
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-01-22 17:39:08 -0300

    add componente style login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Header from '../components/header/Header';
+import Style from './styles/Login.module.css';
 
 const Login = () => {
   const { setNomeUser, nomeUser, setPassword, password } = useContext(Context);
@@ -24,10 +25,10 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <section>
-        <img src={melonImg} alt="imagem de melancia" />
-        <form>
-          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+      <section className={Style.sectionLogin}>
+        <img src={melonImg} alt="imagem de melancia" className={Style.imagemLogin} />
+        <form className={Style.formLogin}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }} className={Style.InputUser}>
             <TextField
               id="input-with-sx"
               label="User"
@@ -36,8 +37,7 @@ const Login = () => {
               required
             />
           </Box>
-          <br />
-          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }} className={Style.InputUser}>
             <TextField
               type="password"
               id="input-with-sx"
@@ -47,21 +47,26 @@ const Login = () => {
               required
             />
           </Box>
-          <br />
-          <a href="https://www.google.com/" target="_blank" rel="noreferrer">
-            <img src="https://img.icons8.com/color/35/000000/google-logo.png" />
-          </a>
-          <a href="https://br.linkedin.com/" target="_blank" rel="noreferrer">
-            <img src="https://img.icons8.com/color/35/000000/linkedin.png" />
-          </a>
-          <a href="https://www.icloud.com/" target="_blank" rel="noreferrer">
-            <img src="https://img.icons8.com/ios-glyphs/35/000000/mac-client.png" />
-          </a>
-          <br />
-          <br />
-          <Button type="submit" onClick={() => handleOnClick()} variant="contained" color="success">
-            Login
-          </Button>
+          <div className={Style.iconesLogin}>
+            <a href="https://www.google.com/" target="_blank" rel="noreferrer">
+              <img src="https://img.icons8.com/color/30/000000/google-logo.png" />
+            </a>
+            <a href="https://br.linkedin.com/" target="_blank" rel="noreferrer">
+              <img src="https://img.icons8.com/color/30/000000/linkedin.png" />
+            </a>
+            <a href="https://www.icloud.com/" target="_blank" rel="noreferrer">
+              <img src="https://img.icons8.com/ios-glyphs/30/000000/mac-client.png" />
+            </a>
+          </div>
+          <div className={Style.buttonLogin}>
+            <Button
+              type="submit"
+              onClick={() => handleOnClick()}
+              variant="contained"
+              color="success">
+              Login
+            </Button>
+          </div>
         </form>
       </section>
     </div>
